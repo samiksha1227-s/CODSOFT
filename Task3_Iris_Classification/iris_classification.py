a83d18f8b4080228146ba5ad82a37235a10ea533
samiksha1227-s <samiksha9csboa@gmail.com>
2026-01-23 17:13:55 +0530

    Add files via upload
    Add final Iris classification python file with model and accuracy

--- Task3_Iris_Classification/iris_classification.py
+++ Task3_Iris_Classification/iris_classification.py
@@ -4,3 +4,15 @@ iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = iris.target
 print(X.head())
+print(y[:5])
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy:",accuracy)
